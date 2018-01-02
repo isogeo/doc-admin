aab117a255921e460f1165b35940f6224db90f6c
Guts <Guts@users.noreply.github.com>
2018-01-02 15:35:09 +0100

    fix plugin parse error

--- Help/fr/book.js
+++ Help/fr/book.js
@@ -1,4 +1,11 @@
 module.exports = {
     root: '.',
     "language": "fr"
+    links: {
+        sidebar: {
+            'Site Isogeo': 'http://www.isogeo.com/fr',
+            'Blog': 'http://blog.isogeo.com',
+            'Application': 'https://app.isogeo.com/'
+        	},
+        }
 }
--- book.js
+++ book.js
@@ -4,22 +4,12 @@ module.exports = {
     root: './Help',
 
     links: {
-        sidebar: {
-            'Site Isogeo': 'http://www.isogeo.com/',
-            'Blog': 'http://blog.isogeo.com',
-            'Application': 'https://app.isogeo.com/'
-        },
         sharing: {
-            'google': true,
-            'facebook': true,
-            'twitter': true,
-            'weibo': false,
             'all': true
-        }
+                }
     },
     plugins: [
         "ga",
-        "customtheme",
         "image-captions",
         "page-toc",
         "timeline",
@@ -34,18 +24,13 @@ module.exports = {
         ga: {
             token: 'UA-22345848-2'
             },
-        customtheme: {
-            css: [
-                '_styles/custom.css'
-                ]
-            },
         "image-captions": {
             "variable_name": "pictures",
             "caption": "Fig. _BOOK_IMAGE_NUMBER_ : _CAPTION_"
             },
         "page-toc": {
             "position": "top",
-            "selector": ".markdown-section h2, .markdown-section h3"
+            "selector": ".markdown-section h2, .markdown-section h3",
                     }
         },
     pdf: {
@@ -60,9 +45,6 @@ module.exports = {
         },
         headerTemplate: '_TITLE_',
         footerTemplate: '_SECTION_'
-    },
-    styles: {
-        custom: '_styles/custom.css'
     },
     variables: {
         version: pkg.version,
